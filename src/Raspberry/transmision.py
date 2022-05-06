d7cc7224bf5243f2421d22af7299dc1695ac356b
Galileo Nano <98213513+galileonano22@users.noreply.github.com>
2022-05-06 16:11:54 +0200

    Subprocess (#1)
    * Valores separados
    
    * Update transmision.py
    
    * Día final (Mas suerte el año que viene)

--- src/Raspberry/transmision.py
+++ src/Raspberry/transmision.py
@@ -3,14 +3,10 @@ import sx126x
 import threading
 import time
 import select
-import termios
-import tty
 import busio, board
 import adafruit_bmp280 as bmp280
 import adafruit_ltr390 as ltr390
 
-old_settings = termios.tcgetattr(sys.stdin)
-tty.setcbreak(sys.stdin.fileno())
 
 node = sx126x.sx126x(serial_num="/dev/ttyS0", freq=433, addr=0, power=22, rssi=False)
 
@@ -20,7 +16,7 @@ bmp = bmp280.Adafruit_BMP280_I2C(i2c, 0x76)
 
 
 def send_deal():
-    get_rec = "0," + "AAA" + " " + str(bmp.temperature)[:7] + " " + str(bmp.pressure)[:8] + " " + str(ltr.uvs)[:4] + " " + str(ltr.light) + " "
+    get_rec = "0," + "AAA" + " " + str(bmp.temperature)[:5] + " " + str(bmp.pressure)[:7] + " " + str(ltr.uvs)[:4] + " " + str(ltr.light) + " "
 
     get_t = get_rec.split(",")
 
@@ -54,6 +50,4 @@ try:
         send_deal()
 
 except:
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
-
-termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+    print("Sa matao")
